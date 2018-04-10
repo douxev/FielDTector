89754114d12b1e37536f76660035018740b2f0af
Metraf <spertin94@gmail.com>
2018-04-10 07:26:51 +0200

    Update Arduino_experiment_photodiode.ino

--- Arduino_experiment_photodiode.ino
+++ Arduino_experiment_photodiode.ino
@@ -1,3 +1,4 @@
+
 #include <Wire.h>
 #include "rgb_lcd.h" //using an LCD RGB backlight
 
@@ -6,7 +7,7 @@ rgb_lcd lcd;
 const int colorR = 0;
 const int colorG = 0;
 const int colorB = 42;
-
+bool bruit_conf = 0;
 void setup() 
 {
     // set up the LCD's number of columns and rows:
@@ -16,23 +17,47 @@ void setup()
 
 void loop() 
 {
-    int pinRead0 = analogRead(A0); // photodiode 1 (first of a 9*9 matrix, experimentating)
+    if (bruit_conf == 0);
+    {
+    lcd.clear();
+    lcd.autoscroll();
+    lcd.setCursor(0,0);
+    lcd.print("Initialisation du bruit");
+    delay(2000);
+    int pinRead0 = analogRead(A0); // photodiode 
     float pVolt0 = pinRead0 / 1024.0 * 5.0;
-
-  if (pVolt0>=4.65) //stupid condition to get used to pVolt0 values
-  {
-    lcd.setCursor(0,0);
-    lcd.print("Not much photons");
+    int bruit = pVolt0;
     
-  }
-  else if (pVolt0<4.65) //same stupid condition
-  {
-    lcd.setCursor(0,0);
-    lcd.print("Light is ON     ");
-  }
-
-    lcd.setCursor(0, 1); //print Voltage value reads
-    lcd.print(pVolt0);
-
+    lcd.setCursor(0, 1); 
+    lcd.print("En Attente");
     delay(500);
+    lcd.setCursor(0, 1); 
+    lcd.print("En Attente.");
+    delay(500);
+    lcd.setCursor(0, 1); 
+    lcd.print("En Attente..");
+    delay(500);
+    lcd.setCursor(0, 1); 
+    lcd.print("En Attente...");
+    delay(500);
+    bruit_conf = 1;
+    }
+    
+    lcd.clear();
+    int pinRead0 = analogRead(A0); // photodiode 
+    float pVolt0 = pinRead0 / 1024.0 * 5.0;
+    int mesure = pVolt0 - bruit;
+    
+    lcd.noAutoscroll();
+    lcd.setCursor(0,0);
+    lcd.print("Mesure obtenue :");
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Bruit :");
+    lcd.setCursor(8,0);
+    lcd.print(bruit);
+    lcd.setCursor(0, 1);
+    lcd.print(mesure);
+    delay(10000);
 }
